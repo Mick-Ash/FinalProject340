26620c936e192c64749a836851077e065260de19
Mick-Ash <ashworth59@live.marshall.edu>
2023-11-15 17:37:00 -0500

    Update11/15/23
    Walking animations for Victoria (player character) added and implemented. Error causes problems, will resolve.  Imported uGUI toolset for inventory.

--- FinalProject340/Assets/Scripts/PlayerMovement.cs
+++ FinalProject340/Assets/Scripts/PlayerMovement.cs
@@ -4,38 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D body;
-    public Animator animator;
+    public float moveSpeed = 5f;
 
-    float horizontal;
-    float vertical;
-    float moveLimiter = 0.7f;
+    public Rigidbody2D rb;
 
-    public float runSpeed = 20.0f;
+    Vector2 movement;
 
-    void Start()
-    {
-        body = GetComponent<Rigidbody2D>();
-    }
+    Animator animator;
+
 
     void Update()
     {
-        // Gives a value between -1 and 1
-        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
-        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
-       // animator.SetFloat("Horizontal", horizontal);
-       // animator.SetFloat("Vertical", vertical);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
-        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
-        {
-            // limit movement speed diagonally, so you move at 70% speed
-            horizontal *= moveLimiter;
-            vertical *= moveLimiter;
-        }
-
-        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);   
     }
 }
